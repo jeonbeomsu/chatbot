f08ec7517aeb1a431f2d5d64166e74fac3d1e1b9
bsjeon <jbs497@naver.com>
2020-06-03 08:16:25 +0900

    asd

--- api/ListCard.js
+++ api/ListCard.js
@@ -1,5 +1,6 @@
 // 전역 변수
 
+const domain = "http://18.223.119.113:3000/";
 
 exports.data = function(req, res) {
     console.log(req.body);
@@ -12,13 +13,13 @@ exports.data = function(req, res) {
             "listCard": {
               "header": {
                 "title": "유영제약 제품 LIST",
-                "imageUrl": "http://18.223.119.113:3000/images/ListCard/list_title.jpg",
+                "imageUrl": domain + "images/ListCard/list_title.jpg",
               },
               "items": [
                 {
                   "title": "디바비바 미디움",
                   "description": "리도카인이 포함된 가교 히알루론산과 히알루론산을 피하에 주입하여 물리적인 수복을 통해 성인의 중간(moderate)에서 깊은(severe) 코입술 주름(nasolabial fold)을 일시적으로 개선하기 위해 사용",
-                  "imageUrl": "http://18.223.119.113:3000/images/ListCard/diva_m.jpg",
+                  "imageUrl": domain + "images/ListCard/diva_m.jpg",
                   "link": {
                     "web": "http://www.yypharm.co.kr/product/product06_detail.asp?c_code1=&c_code2=&pro_code=FB000573&prosort=&block=0&GoTopage=1"
                   }
@@ -26,7 +27,7 @@ exports.data = function(req, res) {
                 {
                   "title": "디바비바 소프트",
                   "description": "리도카인이 포함된 가교 히알루론산과 히알루론산을 피하에 주입하여 물리적인 수복을 통해 성인의 중간(moderate)에서 깊은(severe) 코입술 주름(nasolabial fold)을 일시적으로 개선하기 위해 사용",
-                  "imageUrl": "http://18.223.119.113:3000/images/ListCard/diva_s.jpg",
+                  "imageUrl": domain + "images/ListCard/diva_s.jpg",
                   "link": {
                     "web": "http://www.yypharm.co.kr/product/product06_detail.asp?c_code1=&c_code2=&pro_code=FB000693&prosort=&block=0&GoTopage=1"
                   }
@@ -34,7 +35,7 @@ exports.data = function(req, res) {
                 {
                   "title": "디바비바 하드",
                   "description": "리도카인이 포함된 가교 히알루론산과 히알루론산을 피하에 주입하여 물리적인 수복을 통해 성인의 중간(moderate)에서 깊은(severe) 코입술 주름(nasolabial fold)을 일시적으로 개선하기 위해 사용",
-                  "imageUrl": "http://18.223.119.113:3000/images/ListCard/diva_h.jpg",
+                  "imageUrl": domain + "images/ListCard/diva_h.jpg",
                   "link": {
                     "web": "http://www.yypharm.co.kr/product/product06_detail.asp?c_code1=&c_code2=&pro_code=FB000443&prosort=&block=0&GoTopage=1"
                   }
--- index.js
+++ index.js
@@ -25,7 +25,9 @@ var Carousel = require('./api/Carousel');
 apiRouter.post('/sayHello', say.data);
 apiRouter.post('/sawoninfo', sawoninfo.data);
 apiRouter.post('/BasicCard', BasicCard.data);
+apiRouter.post('/ListCard', ListCard.data);
 apiRouter.post('/Carousel', Carousel.data);
+
 
 
 // static serving 
