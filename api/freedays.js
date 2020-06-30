44c03ed39518b15cd097bc72cce5ed6ded25f243
bsjeon <jbs497@naver.com>
2020-06-30 16:12:48 +0900

    asd

--- api/freedays.js
+++ api/freedays.js
@@ -1,5 +1,5 @@
 exports.data = function(req, res) {
-    var yyyy = req.body.action.params.sys_date_period;
+    var yyyy = req.body.action.params.sys_date_period.year;
     
     var moment = require('moment');
 
